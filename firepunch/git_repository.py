ad3236e0687e61d009170161fd00cc7d2debff0e
Fumitaka Tokumitsu <toku345@gmail.com>
2019-03-24 16:54:34 +0900

    Except a commit at just a full day ago

--- firepunch/git_repository.py
+++ firepunch/git_repository.py
@@ -5,18 +5,17 @@ from datetime import timedelta
 class GitRepository:
     GITHUB_API_URL = "https://api.github.com"
 
-    def __init__(self, access_token, today):
+    def __init__(self, access_token, now):
         self.access_token = access_token
-        self.today = today
+        self.now = now
 
     def change_commits_1_day_before(self):
-        yesterday = self.today - timedelta(days=1)
+        a_day_ago = (self.now - timedelta(days=1)) + timedelta(seconds=1)
         request_uri = f"{self.GITHUB_API_URL}/repos/toku345/firepunch/commits"
-
         params = {
             "access_token": self.access_token,
-            "since": yesterday.strftime("%Y-%m-%dT%H:%M:%SZ"),
-            "until": self.today.strftime("%Y-%m-%dT%H:%M:%SZ")
+            "since": a_day_ago.strftime("%Y-%m-%dT%H:%M:%SZ"),
+            "until": self.now.strftime("%Y-%m-%dT%H:%M:%SZ")
         }
         r = requests.get(request_uri, params=params)
         return r.json()
--- tests/test_git_repository.py
+++ tests/test_git_repository.py
@@ -2,11 +2,27 @@ import os
 from datetime import datetime
 from firepunch.git_repository import GitRepository
 
-def test_change_commits_1_day_before():
-    access_token = os.getenv("GITHUB_ACCESS_TOKEN")
-    today = datetime.strptime("2019-03-23T01:00:00Z", "%Y-%m-%dT%H:%M:%SZ")
 
-    git_repository = GitRepository(access_token, today)
+# FIREPUNCH commits:
+# - 2019-03-24Txx:xx:xxZ
+# - 2019-03-22T01:25:52Z
+# - 2019-03-21T12:42:30Z
+# - 2019-03-21T12:39:58Z
+
+def test_get_1_commit_call_at_just_commit_time():
+    access_token = os.getenv("GITHUB_ACCESS_TOKEN")
+    now = datetime.strptime("2019-03-21T12:39:58Z", "%Y-%m-%dT%H:%M:%SZ")
+
+    git_repository = GitRepository(access_token, now)
 
     result = git_repository.change_commits_1_day_before()
     assert 1 == len(result)
+
+def test_get_0_commit_call_at_after_whole_day():
+    access_token = os.getenv("GITHUB_ACCESS_TOKEN")
+    now = datetime.strptime("2019-03-23T01:25:52Z", "%Y-%m-%dT%H:%M:%SZ")
+
+    git_repository = GitRepository(access_token, now)
+
+    result = git_repository.change_commits_1_day_before()
+    assert 0 == len(result)

e039fb3d4f272c4bb28e40d72f78b7cddf10af05
Fumitaka Tokumitsu <toku345@gmail.com>
2019-03-27 09:52:26 +0900

    Use InquiryPeriod

--- firepunch/git_commits_cli_viewer.py
+++ firepunch/git_commits_cli_viewer.py
@@ -1,11 +1,12 @@
 from datetime import timedelta
 from firepunch.git_repository import GitRepository
+from firepunch.inquiry_period import InquiryPeriod
 
 
 class GitCommitsCliViewer:
     def __init__(self, repo_name, now, access_token):
         self.repo_name = repo_name
-        self.now = now
+        self.until = now
         self.access_token = access_token
 
     def print_commits_1_day_before(self):
@@ -15,13 +16,14 @@ class GitCommitsCliViewer:
             print(f"{commit['message']}")
 
         def print_header():
-            print(f"{len(commits)} commits between {a_day_ago} and {self.now}.")
+            print(f"{len(commits)} commits between {since} and {self.until}.")
 
         def print_header_without_commits():
-            print(f"No commits between {a_day_ago} and {self.now}.")
+            print(f"No commits between {since} and {self.until}.")
 
-        a_day_ago = (self.now - timedelta(days=1)) + timedelta(seconds=1)
-        commits = self.get_commits_until_now(since=a_day_ago)
+        since, _until = InquiryPeriod(until=self.until).a_whole_day()
+        commits = self.get_commits(since)
+
         if not commits:
             print_header_without_commits()
         else:
@@ -39,5 +41,5 @@ class GitCommitsCliViewer:
         git_repository = \
             GitRepository(self.repo_name, self.access_token)
         commits_response = \
-            git_repository.change_commits(since=since, until=self.now)
+            git_repository.change_commits(since=since, until=self.until)
         return [filter(cr) for cr in commits_response]

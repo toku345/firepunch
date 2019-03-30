932edb79b8caa3fc2bf5d87b57cbe3f627c2b71a
Fumitaka Tokumitsu <toku345@gmail.com>
2019-03-30 22:21:23 +0900

    Add --days option for cli.py

--- cli.py
+++ cli.py
@@ -7,14 +7,19 @@ from firepunch.git_commits_cli_viewer import GitCommitsCliViewer
 from firepunch.inquiry_period import InquiryPeriod
 
 
-def main():
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("repo_name",
                         help="GitHub repository name. e.g. toku345/firepunch")
-    args = parser.parse_args()
+    parser.add_argument("--days", type=int, default=1,
+                        help="Number of days to investigate")
+    return parser.parse_args()
 
+
+def main():
+    args = parse_args()
     now = datetime.utcnow()
-    inquiry_period = InquiryPeriod(until=now, days=1)
+    inquiry_period = InquiryPeriod(until=now, days=args.days)
 
     github_access_token = os.getenv("GITHUB_ACCESS_TOKEN")
 

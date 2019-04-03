f31b46ee14fc1b77d3897155230c9a6ddf0e2b96
Fumitaka Tokumitsu <toku345@gmail.com>
2019-04-03 10:29:49 +0900

    Enable to post message to Slack

--- firepunch/slack_notifier.py
+++ firepunch/slack_notifier.py
@@ -2,10 +2,16 @@ import requests
 
 
 class SlackNotifier:
+    SLACK_POST_MESSAGE_URL = "https://slack.com/api/chat.postMessage"
+
+    def __init__(self, token, channel_name):
+        self.token = token
+        self.channel_name = channel_name
+
     def post(self, text):
-        SLACK_FILES_UPLOAD_URL = "http://hoge.com"
         params = {
-            "token": "dummy token",
-            "channels": "dummy channel"
+            "token": self.token,
+            "channel": self.channel_name,
+            "text": text
         }
-        requests.post(url=SLACK_FILES_UPLOAD_URL, params=params)
+        return requests.post(url=self.SLACK_POST_MESSAGE_URL, params=params)
--- tests/test_slack_notifier.py
+++ tests/test_slack_notifier.py
@@ -5,12 +5,19 @@ from firepunch.slack_notifier import SlackNotifier
 def test_post_slack(mocker):
     mocker.patch("requests.post")
 
-    SlackNotifier().post(text="xxxxx")
+    token = "dummy_token"
+    channel_name = "#channel_name"
+    text = \
+        "1 commits between 2019-03-20 12:39:59 and 2019-03-21 12:39:58.\n" + \
+        "------------------------\ndate: 2019-03-21T12:39:58Z\nInitial commit"
 
-    exptected_url = "http://hoge.com"
+    SlackNotifier(token=token, channel_name=channel_name).post(text=text)
+
+    exptected_url = SlackNotifier.SLACK_POST_MESSAGE_URL
     expected_params = {
-        "token": "dummy token",
-        "channels": "dummy channel"
+        "token": token,
+        "channel": channel_name,
+        "text": text
     }
     requests.post.assert_called_once_with(url=exptected_url,
                                           params=expected_params)

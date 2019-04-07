d0b3021ad23da1456ad961625347afb615550cfb
Fumitaka Tokumitsu <toku345@gmail.com>
2019-04-07 21:31:33 +0900

    Add since_local, until_local

--- firepunch/inquiry_period.py
+++ firepunch/inquiry_period.py
@@ -1,11 +1,16 @@
 from datetime import timedelta
+from tzlocal import get_localzone
 
 
 class InquiryPeriod:
     def __init__(self, until, days):
-        self.until = until
         self.days = days
+        self.until = until
         self.since = self.__calcrate_since()
+
+        local_tz = get_localzone()
+        self.since_local = local_tz.localize(self.since)
+        self.until_local = local_tz.localize(self.until)
 
     def __calcrate_since(self):
         return (self.until - timedelta(days=self.days)) + timedelta(seconds=1)
--- tests/test_inquiry_period.py
+++ tests/test_inquiry_period.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from pytz import timezone
+
 from firepunch.inquiry_period import InquiryPeriod
 
 
@@ -22,3 +24,29 @@ def test_2_days():
         datetime.strptime("2019-03-19T12:39:59Z", "%Y-%m-%dT%H:%M:%SZ")
     assert expected_since == inquiry_period.since
     assert now == inquiry_period.until
+
+
+def test_since_local(mocker):
+    local_tz = timezone('Asia/Tokyo')
+    mocker.patch("tzlocal.get_localzone", local_tz)
+
+    now = datetime.strptime("2019-03-21T12:39:58Z", "%Y-%m-%dT%H:%M:%SZ")
+    inquiry_period = InquiryPeriod(until=now, days=1)
+
+    expected_since_local = \
+        local_tz.localize(
+            datetime.strptime("2019-03-20T12:39:59Z", "%Y-%m-%dT%H:%M:%SZ"))
+
+    assert inquiry_period.since_local == expected_since_local
+
+
+def test_until_local(mocker):
+    local_tz = timezone('Asia/Tokyo')
+    mocker.patch("tzlocal.get_localzone", local_tz)
+
+    now = datetime.strptime("2019-03-21T12:39:58Z", "%Y-%m-%dT%H:%M:%SZ")
+    inquiry_period = InquiryPeriod(until=now, days=1)
+
+    expected_until_local = local_tz.localize(now)
+
+    assert inquiry_period.until_local == expected_until_local
